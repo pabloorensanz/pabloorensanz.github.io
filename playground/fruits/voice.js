fb54384c255a817918d7def6b1a31091da51837c
pabloorensanz <pablo.orensanz@gmail.com>
2017-11-24 17:18:10 +0100

    a

--- playground/fruits/voice.js
+++ playground/fruits/voice.js
@@ -1,7 +1,9 @@
 var mensaje = document.getElementById('mensaje'),
 	microfono = document.getElementById('microfono'),
 	transcript = '',
+	final_transcript = false,
 	frutas = document.getElementsByClassName("fruta"),
+	pensando = document.getElementById('pensando'),
 	recognizing = false
 
 if (!('webkitSpeechRecognition' in window)) {
@@ -23,34 +25,28 @@ if (!('webkitSpeechRecognition' in window)) {
 		for (var i = event.resultIndex; i < event.results.length; ++i) {
 			if (event.results[i].isFinal) {
 				transcript += event.results[i][0].transcript;
+				final_transcript = true;
 			} else {
-				interim_transcript += event.results[i][0].transcript;
+				transcript += event.results[i][0].transcript;
 			}
 		}
-		if(transcript) {
-			for (var i = 0; i < frutas.length; i++) {
-				frutas[i].classList.remove("activa");
-			}
-			
-			if(transcript.match(/aguacate/i)) document.getElementById('aguacate').className += " activa";
-			else if(transcript.match(/cereza/i)) document.getElementById('cerezas').className += " activa";
-			else if(transcript.match(/coco/i)) document.getElementById('coco').className += " activa";
-			else if(transcript.match(/fresa/i)) document.getElementById('fresa').className += " activa";
-			else if(transcript.match(/kiwi/i)) document.getElementById('kiwi').className += " activa";
-			else if(transcript.match(/limon/i)) document.getElementById('limon').className += " activa";
-			else if(transcript.match(/manzana/i)) document.getElementById('manzana').className += " activa";
-			else if(transcript.match(/naranja/i)) document.getElementById('naranja').className += " activa";
-			else if(transcript.match(/pera/i)) document.getElementById('pera').className += " activa";
-			else if(transcript.match(/piña/i)) document.getElementById('pina').className += " activa";
-			else if(transcript.match(/sandia/i)) document.getElementById('sandia').className += " activa";
-			else if(transcript.match(/uva/i)) document.getElementById('uvas').className += " activa";
-			else document.getElementById('pensando').style.visibility = "visible";
-			
-			console.log('transcript: '+transcript);
-			transcript = '';
-		} else {
-			console.log('interim: '+interim_transcript);
-		}
+		
+		if(transcript.match(/aguacate/i)) hay_match('aguacate');
+		else if(transcript.match(/cereza/i)) hay_match('cerezas');
+		else if(transcript.match(/coco/i)) hay_match('coco');
+		else if(transcript.match(/fresa/i)) hay_match('fresa');
+		else if(transcript.match(/kiwi/i)) hay_match('kiwi');
+		else if(transcript.match(/limón/i)) hay_match('limon');
+		else if(transcript.match(/manzana/i)) hay_match('manzana');
+		else if(transcript.match(/naranja/i) || transcript.match(/mandarina/i)) hay_match('naranja');
+		else if(transcript.match(/pera/i) || transcript.match(/pere/i)) ;
+		else if(transcript.match(/piña/i)) hay_match('pina');
+		else if(transcript.match(/sandía/i)) hay_match('sandia');
+		else if(transcript.match(/uva/i)) hay_match('uvas');
+		else hay_match('pensando');//no hay match
+		
+		console.log('transcript: '+transcript);
+		transcript = '';
 	}
 	recognition.onerror = function(event) {
 		console.log('Error: '+event.error);
@@ -59,9 +55,17 @@ if (!('webkitSpeechRecognition' in window)) {
 		//no escuchando
 		console.log('Fin');
 		recognizing = false;
-		mensaje.innerHTML = 'Puedes decir el nombre de la fruta que te apecete delante de tu micro.';
+		mensaje.innerHTML = 'Ya hemos jugado. ¿Quieres volver a empezar? Recarga la página.';
 		microfono.classList.remove("pulse");
 	}
+}
+
+function hay_match (id) {
+	for (var i = 0; i < frutas.length; i++) {
+		frutas[i].classList.remove("activa");			
+	}
+	
+	document.getElementById(id).className += " activa";
 }
 
 function escuchar () {
