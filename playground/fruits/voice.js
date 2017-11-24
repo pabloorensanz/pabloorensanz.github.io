75f08969ef766005919abf43ee98d7924d1e1ee1
pabloorensanz <pablo.orensanz@gmail.com>
2017-11-24 18:23:41 +0100

    a

--- playground/fruits/voice.js
+++ playground/fruits/voice.js
@@ -65,7 +65,7 @@ function hay_match (id) {
 	transcript = '';
 	//resultado.classList.remove('activo');
 	console.log('mostrar: '+id);
-	resultado.src = 'multimedia/'+id+'.jpg';
+	resultado.src = 'multimedia/'+id+'.png';
 	resultado.className += ' activo';
 }
 
